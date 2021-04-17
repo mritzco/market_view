bbae0f5e75d2699a2a0bc972d4b2c06fb0a272f2
itzco <4988324+mritzco@use>
2021-04-17 17:12:24 +0700

    Force eth/btc on top

--- public/linkshack.js
+++ public/linkshack.js
@@ -1,7 +1,7 @@
 function linktop(rows) {
   // console.log(rows);
   const link =
-    "https://cryptowatch.net/?chart=BINANCE:BTCUSDTPERP&chart=BINANCE:ETHUSDTPERP&" +
+    "https://cryptowatch.net/?" +
     rows.map(s => `chart=BINANCE:${s.toUpperCase()}USDTPERP`).join("&");
   document.getElementById("chartslink").href = link;
   document.getElementById("chartslink").target = "_blank";
